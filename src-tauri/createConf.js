53e82d16027f0cf6002b4334e06c2bc4a996a5a0
piaoyidage <longyixiaoge@163.com>
2022-10-31 09:26:02 +0800

    test

--- src-tauri/createConf.js
+++ src-tauri/createConf.js
@@ -80,9 +80,13 @@ const configList = [{
     identifier: "com.tw93.flomo"
 }]
 
+execSync('mkdir /src-tauri/target/release/bundle/dmg-backup/')
 
 configList.forEach(c => {
     const config = JSON.stringify(createConf(c));
     fs.writeFileSync(path.resolve(__dirname, 'tauri.conf.json'), config)
     execSync(`npm run tauri build`)
+    execSync('mv /src-tauri/target/release/bundle/dmg/*.dmg /src-tauri/target/release/bundle/dmg-backup/')
 })
+
+execSync('mv /src-tauri/target/release/bundle/dmg-backup/*.dmg /src-tauri/target/release/bundle/dmg/')
